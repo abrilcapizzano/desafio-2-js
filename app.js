13334eed323708801a78e658a66731cb358cf273
abrilcapizzano <103966017+abrilcapizzano@users.noreply.github.com>
2022-07-26 15:46:03 -0300

    cambios

--- app.js
+++ app.js
@@ -1,35 +1,39 @@
-let resultado = 0
-function conversor () {
-  let destino = prompt ('Ingrese el destino: Rio, Londres o Perú ').toLowerCase() 
-  let dias = Number (prompt ('Ingrese la cantidad de días'))
-  let adultos = Number (prompt('Ingrese la cantidad de adultos'))
-  let menores = Number (prompt('Ingrese la cantidad de niños'))
-let suma = (dias * 5000) + (adultos * 20000) + (menores * 10000)
-   if(destino ===  'rio'){
-      resultado = suma + 100000
-     }
-else if(destino === 'londres'){
-      resultado = suma + 170000
-    }
-else if(destino === 'perú'){
-      resultado = suma + 140000
+let compra = confirm('Desea comprar un pasaje?')
+if (compra) {
+  calcularViaje()
 }else{
-  alert('Ese destino no existe')
-  let destino = prompt ('Ingrese el destino: Rio, Londres o Perú ').toLowerCase()
-   if(destino ===  'rio'){
-    resultado = suma + 100000
-   }
-else if(destino === 'londres'){
-    resultado = suma + 170000
+  alert('Hasta luego')
+}
+function calcularViaje() {
+  let resultado = 0;
+  let destino;
+  let suma;
+  let cuotas;
+  do {
+    destino = prompt('Elejí tu destino: Rio, Londres o New York').toLowerCase()
+    let dias = Number(prompt('Ingrese la cantidad de días'));
+    let adultos = Number(prompt('Ingrese la cantidad de adultos'));
+    let menores = Number(prompt('Ingrese la cantidad de niños'))
+    suma = dias * 15000 + adultos * 25000 + menores * 15000;
+    if (!(destino == 'rio' || destino == 'londres' || destino == 'new york') || isNaN(suma)) {
+      alert('datos ingresados incorrectos, intenta nuevamente')
+    }
+  } while (!(destino == 'rio' || destino == 'londres' || destino == 'new york') || isNaN(suma));
+  if (destino === 'rio') {
+    mostrarResultado(100000)
+  } else if (destino === 'londres') {
+    mostrarResultado(200000)
+  } else if (destino === 'new york') {
+    mostrarResultado(250000)
   }
-else if(destino === 'perú'){
-    resultado = suma + 140000
-}
-}}
-function mostrar() {
-  alert (resultado + ' pesos')
-}
-console.log (conversor)
-console.log (mostrar)
-conversor()
-mostrar(resultado)
+  function mostrarResultado(precioPasaje) {
+    alert(resultado + suma + precioPasaje + ' pesos es el valor de tu viaje')
+    cuotas = confirm('Desea pagar en 3 cuotas?')
+    if (cuotas) {
+      alert((resultado + suma + precioPasaje) / 3 + ' pesos es el valor de cada cuota')
+    }else{
+      alert('Hasta luego')
+    }
+  }
+  }
+
